14fd4d349d4fd33861eddff1a37561391aa0326d
Marcos Mtz <marcos.the.silencer@gmail.com>
2021-11-30 18:39:03 -0600

    Añadiendo las primeras clases (persona y fecha) para comenzar a crear la base de datos

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-let faker = require('faker');
+let faker = require('faker/locale/es_MX');
+faker.locale = "es_MX";
 
-//console.log(faker);
+//console.log(`${faker.name.firstName()} ${faker.name.middleName()} ${faker.name.lastName()} ${faker.name.lastName()}`);
+
+class Fecha {
+    constructor(dia = 1, mes = 1, año =) {
+        this.date = new Date();
+    }
+}
+
+function Persona() {
+    this.nombres
+    this.apellidoMaterno
+    this.apellidoPaterno
+    this.fechaNacimiento
+}
