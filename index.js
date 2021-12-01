f88c0310243c38e4445bc24012724d34ef14d54c
Marcos Mtz <marcos.the.silencer@gmail.com>
2021-11-30 19:02:13 -0600

    Primera implementacion de PersonaAlAzar

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ faker.locale = "es_MX";
 //console.log(`${faker.name.firstName()} ${faker.name.middleName()} ${faker.name.lastName()} ${faker.name.lastName()}`);
 
 class Fecha {
-    constructor(dia = 1, mes = 1, año =) {
-        this.date = new Date();
+    constructor(dia = 1, mes = 1, año = 1970) {
+        this.date = new Date(año, mes, dia);
     }
 }
 
-function Persona() {
-    this.nombres
-    this.apellidoMaterno
-    this.apellidoPaterno
-    this.fechaNacimiento
+class PersonaAlAzar {
+    constructor() {
+        this.nombres = this.getNombres();
+        this.apellidoMaterno = faker.name.lastName();
+        this.apellidoPaterno = faker.name.lastName();
+        this.fechaNacimiento = faker.date.past(60);
+    }
+    getNombres() {
+        return faker.name.firstName();
+    }
+    getPersonaTexto() {
+        return `Nombre: ${this.nombres} ${this.apellidoMaterno} ${this.apellidoPaterno}\nFecha de nacimiento: ${this.fechaNacimiento}\n`;
+    }
 }
+
+console.log( (new PersonaAlAzar()).getPersonaTexto() );
+console.log( (new PersonaAlAzar()).getPersonaTexto() );
+console.log( (new PersonaAlAzar()).getPersonaTexto() );
+console.log( (new PersonaAlAzar()).getPersonaTexto() );
